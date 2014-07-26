4333116219a9e5af6c26f532ebaf8143f8b10b68
Chani Armitage <chanika@gmail.com>
2014-07-26 15:29:28 -0700

    first run at analysis logic; super-buggy.
    it seems to be using text addition sometimes, and math other times.
    whee! :P

--- app.js
+++ app.js
@@ -13,14 +13,33 @@ app.get('/', function(req, res){
 });
 
 app.post('/analysis', function (req, res) {
-  var data = req.body;
-  data.partner = 123;
-  data.addI = 321;
-  data.addV = -5;
-  //console.log("data:");
+  var data = analyze(req.body);
   console.log(data);
   res.render('analysis', data);
 });
 
-
 app.listen(PORT);
+
+
+//business logic ain't big enough for its own file yet
+
+function analyze(data) {
+  //bills A and H come from account I.
+  //bill V comes from account V
+  //partner pays into account V
+  //bills A and H are shared; sharedV and other are also shared costs for now (from V and other sources)
+  //^^^ TODO: put that logic into a json file
+  
+  console.log(data);
+  //TODO: make the math currency-safe
+  var shared = data.billA + data.billH + data.sharedV + data.other;
+  data.partner = shared/2;
+  
+  var debitI = data.billA + data.billH;
+  var debitV = data.billV - data.partner;
+  
+  data.addI = debitI - data.accountI;
+  data.addV = debitV - data.accountV;
+  
+  return data;
+}
