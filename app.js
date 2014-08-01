a16935506012d512ba31affec7f2a852a2bf0288
Chani Armitage <chanika@gmail.com>
2014-08-01 13:37:06 -0700

    Ensure inputs are converted to numbers

--- app.js
+++ app.js
@@ -23,15 +23,27 @@ app.listen(PORT);
 
 //business logic ain't big enough for its own file yet
 
-function analyze(data) {
+function analyze(input) {
   //bills A and H come from account I.
   //bill V comes from account V
   //partner pays into account V
   //bills A and H are shared; sharedV and other are also shared costs for now (from V and other sources)
   //^^^ TODO: put that logic into a json file
   
-  console.log(data);
-  //TODO: make the math currency-safe
+  console.log(input);
+  //mathify all potential strings
+  var data = {
+    billA: toCurrency(input.billA),
+    billH: toCurrency(input.billH),
+    billV: toCurrency(input.billV),
+    sharedV: toCurrency(input.sharedV),
+    other: toCurrency(input.other),
+    accountI: toCurrency(input.accountI),
+    accountV: toCurrency(input.accountV)
+  };
+  
+  //note: there's still a chance of small rounding errors here, but they should be way less than a penny
+  //and since I round my money transfers up to the nearest $10, I don't care.
   var shared = data.billA + data.billH + data.sharedV + data.other;
   data.partner = shared/2;
   
@@ -42,4 +54,9 @@ function analyze(data) {
   data.addV = debitV - data.accountV;
   
   return data;
+}
+
+function toCurrency(input) {
+  //parseFloat returns NaN for blanks, in which case we want 0.
+  return parseFloat(input) || 0;
 }
