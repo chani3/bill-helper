696ddd9c4462ec99ddfbf1a3ec9a7e1821642f01
Chani Armitage <chanika@gmail.com>
2015-08-10 14:31:43 -0700

    Use jq to get the port from settings.json

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ node app.js &
 nodePid=$!
 echo pid is $nodePid
 trap "echo killing node...; kill $nodePid" EXIT
-#TODO get port from config
-firefox --new-window http://localhost:3001/
+
+port=$(jq '.port' settings.json)
+firefox --new-window http://localhost:$port/
 wait $nodePid
